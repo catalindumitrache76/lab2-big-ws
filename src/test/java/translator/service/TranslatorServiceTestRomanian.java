b9c0bedee3c52307bd969db4ef5a958912717fad
catalindumitrache76 <catalindumitrache76@yahoo.es>
2016-10-28 20:41:38 +0200

    Added spanish to romanian translation test

---
+++ src/test/java/translator/service/TranslatorServiceTestRomanian.java
@@ -0,0 +1,27 @@
+package translator.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import translator.Application;
+import translator.domain.TranslatedText;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class TranslatorServiceTestRomanian {
+
+    @Autowired
+    TranslatorService translatorService;
+    @Test
+    public void translateTest() throws Exception {
+        TranslatedText translatedText = translatorService.translate("es", "ro", "Esto es una prueba para el servicio de traducción");
+        assertEquals("Acesta este un test pentru serviciul de traducere",translatedText.getTranslation());
+    }
+
+}
